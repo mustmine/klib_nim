71a8415be1ceff18726cf4145d18a1f4c1a35d21
mustmine <mineqwwwa@outlook.com>
2024-10-14 22:27:20 +0800

    more example

--- example/kt_for.nim
+++ example/kt_for.nim
@@ -1,11 +1,19 @@
 import klib_nim/kthread
 
+# simple for with no data operation
+proc say() {.cdecl.} = echo "hello"
+let echoTiems = 5
+
+kt_for(2, cast[ptr KtForFn](say), nil, echoTiems.clong)
+
+
+# with  data operation
 var a = [1, 2, 3, 4]
 
-proc squared(data: pointer, index: clong, thread_id: cint) {.cdecl.} =
+proc square(data: pointer, index: clong, thread_id: cint) {.cdecl.} =
     var arr = cast[ptr array[4, int]](data)
     arr[][index.int] = arr[][index.int] * arr[][index.int]
 
-    echo "thread_id: ", thread_id, ", array_index: ", index, ", value: ", arr[][index.int]
+    echo "thread_id: ", thread_id, ", array_index: ", index, ", square_value: ", arr[][index.int]
     
-kt_for(2, cast[ptr KtForFn](squared), a.addr, a.len.clong)
+kt_for(2, cast[ptr KtForFn](square), a.addr, a.len.clong)
