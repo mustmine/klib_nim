29e40428102fe921cb57f032b9dac5dd6e0efd9e
mineqwwe <mineqwwwa@outlook.com>
2024-10-14 15:26:27 +0800

    init kthread

---
+++ klib_nim.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "mustmine"
+description   = "simple wrapper for klib"
+license       = "MIT"
+srcDir        = "src"
+
+
+# Dependencies
+
+
---
+++ src/klib_nim/kthread.nim
@@ -0,0 +1,15 @@
+{.compile: "klib/kthread.c".}
+
+# kt_for()
+type KtForFn = proc (data: pointer, index: clong, thread_id: cint){.cdecl.}
+proc kt_for(n_threads: cint, fn: ptr KtForFn, data: pointer, n_works: clong) {.cdecl, importc: "kt_for", header: "klib/kthread.h".}
+
+# kt_pipeline()
+type KtPipelineFn = proc (shared_data: pointer, step: cint, in_data: pointer){.cdecl.}
+proc kt_pipeline(n_threads: cint, fn: ptr KtPipelineFn, shared_data: pointer, n_steps: cint) {.cdecl, importc: "kt_pipeline", header: "klib/kthread.h".}
+
+# kt_for() with thread pool
+type ThreadPool = pointer
+proc kt_forpool_init(n_threads: cint): ThreadPool{.cdecl, importc: "kt_forpool_init", header: "klib/kthread.h".}
+proc kt_forpool_destroy(pool: ThreadPool) {.cdecl, importc: "kt_forpool_destroy", header: "klib/kthread.h".}
+proc kt_forpool(pool: ThreadPool, fn: ptr KtForFn, data: pointer, n_works: clong) {.cdecl, importc: "kt_forpool", header: "klib/kthread.h".}
